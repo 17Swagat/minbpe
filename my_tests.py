2b6b347f44d66ff298e516499748b5e981ede84c
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2026-03-15 13:45:59 +0530

    Testing the basic tokenizer

--- my_tests.py
+++ my_tests.py
@@ -1,43 +1,39 @@
 """This file will contains code for testing my `Version of MinBPE`"""
 
 # %%
-# Let's Test out `minbpe`:=>
-
-# %%
 # My MinBPE Version
 from my_minbpe import BasicTokenizer
+mytokenizer = BasicTokenizer()
 
-tokenizer = BasicTokenizer()
-  
-# % %
-# tokenizer.train("aaabdaaabac", vocab_size=300, verbose=True)
-# print(tokenizer.merges)
-# Training on the Taylor Swift.txt
-with open('./tests/taylorswift.txt', 'r') as f:
-    text = f.read()
-print(len(text))
-print(len(list(text.encode('utf-8'))))
+with open('tests/taylorswift.txt', 'r') as file:
+    text = file.read()
 
-# % %
-tokenizer.train(text, vocab_size=500, verbose=False)
-
-# % %
-txt = "Sept album September"
-my_encoding = tokenizer.encode(txt)
-my_decoded_text = tokenizer.decode(my_encoding)
+mytokenizer.train(text, 500, False)
+# %%
+inputText = 'Awation    '
+my_encoding = mytokenizer.encode(inputText)
 print(my_encoding)
-print(my_decoded_text)
-print(txt == my_decoded_text)
+my_decoding = mytokenizer.decode(my_encoding)
+print(my_decoding == inputText)
+# %% 
+idx_check = 468 #388 #279 #32
+print(
+    mytokenizer.vocab[idx_check]
+)
+# %%
+inputText = 'write a album'
+my_encoding = mytokenizer.encode(inputText)
+print(my_encoding)
+my_decoding = mytokenizer.decode(my_encoding)
+print(my_decoding == inputText)
+# %%
+for enc in my_encoding:
+    print(f'{enc} -> {mytokenizer.vocab[enc]}')
 
 # %%
-    
-# %%
-print(tokenizer.decode([469, 470, 472]))
-    
-# %%
-tokenizer.vocab
-
+mytokenizer.vocab
 
 # %%
-print('hello')
-# %%
+# @Karpathy:
+# Let's Test out `minbpe`:=>
+
